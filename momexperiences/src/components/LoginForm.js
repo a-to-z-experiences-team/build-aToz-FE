a2eee12446f2bc024d0848a55107456ef4740a9e
Michael Jenkins <mjenkins9605@vidangel.com>
2019-04-25 00:00:46 -0400

    added login form

---
+++ momexperiences/src/components/LoginForm.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { connect } from "react-redux";
+import { login } from "../actions";
+
+class LoginForm extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+  login = event => {
+    event.preventDefault();
+    this.props.login(this.state.credentials).then(() => {
+      this.props.history.push("/experiencelist");
+    });
+  };
+  handleChange = event => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [event.target.name]: event.target.value
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="login-page-wrapper">
+        <h2>Login</h2>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+            required
+          />
+          <button value="isLoggingIn">submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isLoggingIn: state.isLoggingIn
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { login }
+)(LoginForm);
