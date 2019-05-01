9af17784e297bc390972f56bc4a354fc0bd43c13
Michael Jenkins <mjenkins9605@vidangel.com>
2019-05-01 09:08:33 -0400

    missed typo

--- momexperiences/src/index.js
+++ momexperiences/src/index.js
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(reducer, applyMiddleware(thunk,logger)
